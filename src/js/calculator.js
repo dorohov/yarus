b8d6059a3bf7cd10cc572e75953644cb0ce1c4d8
Pavel Proyavin <pavel@dorohovdesign.com>
2019-03-22 17:59:01 +0300

    Changelog from 220319

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,16 +1,32 @@
 (function($) {
     "use strict"
     $(function() {
+
+        var
+            step1_price =   0,
+            step2_price =   0,
+            step3_meters =  0,
+            current_step =  1,
+            ruble = $('input#calc31').val(),
+            total = 0,
+            titles = [
+                '1. Выберите тип дома:',
+                '2. Выберите тип ремонта:',
+                '3. Производится расчет стоимости ремонта, наш менеджер Вас подробно проконсультирует. Оставьте номер телефона для связи.'
+            ],
+            titleBlock = $('.calculator__right__subtitle')
+
+        function setStep(num) {
+            titleBlock.html(titles[current_step - 1])
+            $('.calculator__right__step').removeClass('is--active')
+            $('.calculator__right__step[data-stepid="' + num +'"]').addClass('is--active')
+        }
         
+        setStep(current_step)
+
         $('.calculator__btn button[type="submit"]').on('click', function(e) {
             
             e.preventDefault()
-
-            var
-                step1_price =   0,
-                step2_price =   0,
-                step3_meters =  0,
-                current_step =  1
 
             if(current_step == 1) {
 
@@ -22,14 +38,57 @@
                     else if(inputs.attr('id') == 'calc13') step1_price = 1
 
                     if(step1_price > 0) {
-                        console.log('Идем дальше')
+                        current_step = 2
+                        setStep(current_step)
                     }
                 }
 
-            }else if(current_step == 2) {
+            }
+            else if(current_step == 2) {
+
+                var inputs = $('.calculator__right__steps').find('input[name="calc2"]:checked')
+
+                if(inputs.length > 0) {
+                    if(inputs.attr('id') == 'calc21') {
+                        step2_price = 7500
+                    }else if(inputs.attr('id') == 'calc22') {
+                        step2_price = 12500
+                    }
+
+                    if(step2_price > 0 && $('input[id="calc23"]').val() != null && $('input[id="calc23"]').val() != 0) {
+
+                        total = step1_price * step2_price * $('input[id="calc23"]').val()
+                        $('input#calc31').val(total.toLocaleString('ru-RU') + ruble)
+                        current_step = 3
+                        setStep(current_step)
+                    }
+                }
 
             }
+            else if(current_step == 3) {
 
+                if($('input#calc32').val().length > 10) {
+                    $.ajax({
+                        url: 'form.php',
+                        success: function(data) {
+                        if(data == 'true') {
+                            $('#modal_calculator').modal({
+                                showClose: false,
+                                show: true,
+                                fadeDuration: 100
+                            })
+                        }
+                        }
+                    }); 
+                }
+                 
+            }
+
+        })
+
+        $('.calculator__btn.is--prev').on('click', function() {
+            if(current_step > 0) current_step--
+            setStep(current_step)
         })
 
     })
