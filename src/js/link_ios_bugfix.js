d791c98601ba888f028ce7215be163a22821b7f1
Pavel Proyavin <pavel@dorohovdesign.com>
2019-03-29 17:39:40 +0300

    Fixe links href undefined bug

--- src/js/link_ios_bugfix.js
+++ src/js/link_ios_bugfix.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-    $("a").on("click touchend", function(e) {
-      var el = $(this);
-      var link = el.attr("href");
+  $("a").on("click touchend", function(e) {
+    var el = $(this);
+    var link = el.attr("href");
+    if(!$(el).hasClass('__nohref')) {
       window.location = link;
-    });
+    }
   });
+});
