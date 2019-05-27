ff738973d949e60f7fed27502b46f8d96d6da6b4
Pavel Proyavin <pavel@dorohovdesign.com>
2019-05-27 14:46:10 +0300

    fixes links target blank bug

--- src/js/link_ios_bugfix.js
+++ src/js/link_ios_bugfix.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("a").on("click touchend", function(e) {
     var el = $(this);
     var link = el.attr("href");
-    if(!$(el).hasClass('__nohref')) {
+    if(!$(el).hasClass('__nohref') && !$(el).attr('target')) {
       window.location = link;
     }
   });
