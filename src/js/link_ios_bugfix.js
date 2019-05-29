490586e8a3e9dff503b4243fd325d0722722e6c5
Pavel Proyavin <pavel@dorohovdesign.com>
2019-05-29 17:40:23 +0300

    fixes links bug, fixes portfolio blocks size

--- src/js/link_ios_bugfix.js
+++ src/js/link_ios_bugfix.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-  $("a").on("click touchend", function(e) {
+  $("a.btn, .navbar_menu ul li a, .navbar__mobile__menu ul li a").on("click touchend", function(e) {
     var el = $(this);
     var link = el.attr("href");
-    if(!$(el).hasClass('__nohref') && !$(el).attr('target') && !$(el).attr(data-fancybox)) {
+    if(!$(el).hasClass('__nohref') && 
+    !$(el).attr('target') && 
+    !$(el).attr(data-fancybox) &&
+    !$(el).hasClass('modal__close')
+    ) {
       window.location = link;
     }
   });
