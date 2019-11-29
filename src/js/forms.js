0ddc79c099785b2d17725ba758182d1e4fe64a7e
Pavel Proyavin <pavel@dorohovdesign.com>
2019-11-29 11:42:18 +0300

    release from 291119 | vacancies page, banner, navbar and reviews widget

--- src/js/forms.js
+++ src/js/forms.js
@@ -12,5 +12,13 @@
 
         $('form').parsley()
 
+        $('.form__file input[type="file"]').on('change', function(e) {
+            var name = e.target.files[0].name
+
+            $(this).siblings('label').find('span').html(name)
+
+            console.log($(this).siblings('label').find('span'))
+        })
+
     })
 })(jQuery);
